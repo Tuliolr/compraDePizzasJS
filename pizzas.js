e76b535518b8b0e51452a613d467e03db3243b18
Tulio <tuliolopesr@gmail.com>
2020-02-07 17:25:36 -0300

    Subindo projeto no Git.

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Molho, porção extra mussarela ralada, tomate, azeitona e orégano'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Molho, mussarela, calabresa fatiada em rodela, tomate, azeitona e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Molho, mussarela, catupiry, provolone fatiado, gorgonzola, tomate, azeitona e orégano'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho, mussarela, presunto ralado, bacon em cubos milho, tomate, azeitona e orégano.'},
+    {id:5, name:'Portuguesa', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Molho, mussarela, presunto ralado, ovo, palmito, ervilha, tomate, azeitona e orégano'},
+    {id:6, name:'Marguerita', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Molho, mussarela, manjericão fresco, parmesão ralado, tomate, azeitona e orégano'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Molho, musarela, presunto ralado, ovo, palmito, ervilha, tomate, azeitona e orégano'}
+];
